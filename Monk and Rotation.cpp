33e0ee3b1a1459f86faabd1a72a8d1ae025d5779
ameer salman <46555564+Ameersalman123@users.noreply.github.com>
2020-09-18 19:31:11 +0530

    Create Monk and Rotation.cpp

---
+++ Monk and Rotation.cpp
@@ -0,0 +1,52 @@
+/*Monk loves to preform different operations on arrays, and so being the principal of Hackerearth School, he assigned a task to his new student Mishki. Mishki will be provided with an integer array A of size N and an integer K , where she needs to rotate the array in the right direction by K steps and then print the resultant array. As she is new to the school, please help her to complete the task.
+
+Input:
+The first line will consists of one integer T denoting the number of test cases.
+For each test case:
+1) The first line consists of two integers N and K, N being the number of elements in the array and K denotes the number of steps of rotation.
+2) The next line consists of N space separated integers , denoting the elements of the array A.
+Output:
+Print the required array.
+
+Constraints:
+
+
+
+
+Sample Input
+1
+5 2
+1 2 3 4 5
+Sample Output
+4 5 1 2 3
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;
+		int a[n];
+		while(k>=n)
+			k=k-n;
+		for(int i=0;i<n;i++)
+		{
+			if(i+k<n)
+				cin>>a[i+k];
+			else
+				cin>>a[k+i-n];
+		}
+		for(int i=0;i<n;i++)
+		{
+			cout<<a[i]<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
